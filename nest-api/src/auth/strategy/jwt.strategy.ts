a03010bbc0e62f2062a5da57af858b78824731b9
ioannis-tsagki <ioannistsagkaris@gmail.com>
2023-11-30 15:55:52 +0200

    jwt, guards, decorators

--- nest-api/src/auth/strategy/jwt.strategy.ts
+++ nest-api/src/auth/strategy/jwt.strategy.ts
@@ -2,17 +2,29 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
-	constructor(config: ConfigService) {
+	constructor(
+		config: ConfigService,
+		private prisma: PrismaService,
+	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			secretOrKey: config.get("JWT_SECRET"),
 		});
 	}
 
-	validate(payload: any) {
-		return payload;
+	async validate(payload: { email: string; password: string }) {
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: payload.email,
+				hash: payload.password,
+			},
+		});
+		delete user.hash;
+		delete user.id;
+		return user;
 	}
 }
