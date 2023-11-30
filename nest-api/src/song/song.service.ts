235f6cf05b0f3b6340157bea171c96c09f2f8825
ioannis-tsagki <ioannistsagkaris@gmail.com>
2023-11-30 19:35:48 +0200

    Update song.service.ts

--- nest-api/src/song/song.service.ts
+++ nest-api/src/song/song.service.ts
@@ -2,21 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { SongDto } from "./dto";
 import { AlbumDto } from "src/album/dto";
-<<<<<<< HEAD
-import { ArtistDto } from "src/artist/dto";
-
-@Injectable()
-export class SongService {
-	constructor(private prisma: PrismaService) {}
-
-	AddSongs(dto: SongDto, albumdto: AlbumDto, artistdto: ArtistDto) {
-		const song = this.prisma.song.create({
-			data: {
-				idAlbum: albumdto.name,
-				idArtist: artistdto.name,
-				name: dto.name,
-				//genre: dto.genre,
-=======
 import { ArtistService } from "src/artist/artist.service";
 
 @Injectable()
@@ -33,7 +18,6 @@ export class SongService {
 				idArtist: await this.artist.id,
 				name: dto.name,
 				genre: dto.genre,
->>>>>>> a393b95a6ee738791ed9e4f22d6ff5e3b12129bc
 				track: dto.track,
 			},
 		});
