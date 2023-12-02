1e0722d70a0bbb19f0a50a639df1f8e92a3658bf
ioannis-tsagki <ioannistsagkaris@gmail.com>
2023-12-02 18:36:36 +0200

    Delete admin.service.ts

--- nest-api/src/admin/admin.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@nestjs/common";
-/*import { ArtistController } from "src/artist/artist.controller";
-
-@Injectable()
-export class AdminService {
-	constructor(private artist: ArtistController) {}
-}*/
