900b1951eeb90745e3110cc2aa1f55e2381c2cf8
ioannis-tsagki <ioannistsagkaris@gmail.com>
2024-02-09 17:50:17 +0200

    Update Search.tsx

--- react-native-app/screens/Search.tsx
+++ react-native-app/screens/Search.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from "../components/Theme";
+import { useState } from 'react';
 
 
 export default function Search() {
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearch = () => {
+        console.log('Search text:', searchText);
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Search</Text>
+            <TextInput
+                style={styles.textInput}
+                placeholder="Artists, albums, songs"
+                onChangeText={setSearchText}
+                value={searchText}
+            />
+            <TouchableOpacity onPress={handleSearch}>
+                <Text style={styles.buttonText}>cancel</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -13,5 +28,22 @@ export default function Search() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    textInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 2,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 70,
+        paddingHorizontal: 10,
+        borderRadius: 20,
+    },
+    buttonText: {
+        marginLeft: 330,
+        marginTop: -28,
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: '#19bfb7',
     },
 })
