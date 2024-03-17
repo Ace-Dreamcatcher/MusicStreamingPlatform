fa2736f3ecddf4e592ce7ac69ca866f9d81bd634
ioannis-tsagki <ioannistsagkaris@gmail.com>
2024-03-17 15:06:43 +0200

    tests

--- nest-api/test/tests/auth.controller.int.spec.ts
+++ nest-api/test/tests/auth.controller.int.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
-import { SignUpDto } from 'src/auth/dto';
+import { SignInDto, SignUpDto, TokenDto, UpdateDto } from 'src/auth/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import * as argon from 'argon2';
 import { BadRequestException } from '@nestjs/common';
+import * as argon from 'argon2';
 
 describe('AuthController', () => {
 	let controller: AuthController;
@@ -12,18 +12,21 @@ describe('AuthController', () => {
 	const mockPrismaService = {
 		user: {
 			create: jest.fn(),
+			findUnique: jest.fn(),
+			update: jest.fn(),
+			delete: jest.fn(),
 		},
 	};
 
 	const mockAuthService = {
 		signup: jest.fn(async (dto: SignUpDto) => {
-			(argon as any).hash = jest.fn().mockResolvedValue('hashedPassword');
+			(argon as any).hash = jest.fn().mockResolvedValue(dto.password);
 
-			if (dto.email === 'registeredUser@gmail.com') {
+			if (dto.email === 'Ioannis@gmail.com') {
 				throw new BadRequestException('Email is already signed in!');
 			}
 
-			if (dto.email === 'invalid-email') {
+			if (dto.email === 'invalidEmail') {
 				throw new BadRequestException('Other error!');
 			}
 
@@ -31,12 +34,87 @@ describe('AuthController', () => {
 				id: 'someUserId',
 				email: dto.email,
 				username: dto.username,
-				hash: 'hashedPassword',
+				hash: '$argon2id$v=19$m=65536,t=3,p=4$COaT0vbhjCETwq8/b503Kg$OSIxLiy4PHDmK2DIwBS/4lZ+qN1StQHAq7v9K7j4ijE',
 				createAt: new Date(),
 				role: 'FREE',
 			});
 
-			return { accessToken: '' };
+			return { accessToken: 'mockedToken' };
+		}),
+
+		signin: jest.fn(async (dto: SignInDto) => {
+			const user = mockPrismaService.user.findUnique.mockResolvedValue({
+				where: {
+					email: dto.email,
+				},
+			});
+
+			(argon as any).verify = jest.fn().mockResolvedValue(dto.password);
+
+			if (!user || dto.email === 'invalidEmail') {
+				throw new BadRequestException('Incorrect email or password!');
+			}
+
+			return { accessToken: 'mockedToken' };
+		}),
+
+		update: jest.fn(async (dto: UpdateDto) => {
+			const mockUser = {
+				id: 'someUserId',
+				email: 'Ioannis@gmail.com',
+				username: 'Ioannis',
+				hash: '$argon2id$v=19$m=65536,t=3,p=4$COaT0vbhjCETwq8/b503Kg$OSIxLiy4PHDmK2DIwBS/4lZ+qN1StQHAq7v9K7j4ijE',
+				createdAt: undefined,
+				role: 'FREE',
+			};
+
+			const user = mockPrismaService.user.findUnique.mockResolvedValue({
+				where: {
+					email: mockUser.email,
+				},
+			});
+
+			if (!user || dto.newEmail === 'invalidEmail') {
+				throw new BadRequestException('There was an error!');
+			}
+
+			mockPrismaService.user.update.mockResolvedValue({
+				where: {
+					email: mockUser.email,
+				},
+				data: {
+					email: dto.newEmail,
+					username: dto.newUsername,
+					hash: ((argon as any).hash = jest
+						.fn()
+						.mockResolvedValue(dto.newPassword)),
+				},
+			});
+
+			return { accessToken: 'mockedToken' };
+		}),
+
+		delete: jest.fn(async (dto: TokenDto) => {
+			const mockUser = {
+				id: 'someUserId',
+				email: 'Ioannis@gmail.com',
+				username: 'Ioannis',
+				hash: '$argon2id$v=19$m=65536,t=3,p=4$COaT0vbhjCETwq8/b503Kg$OSIxLiy4PHDmK2DIwBS/4lZ+qN1StQHAq7v9K7j4ijE',
+				createdAt: undefined,
+				role: 'FREE',
+			};
+
+			const user = mockPrismaService.user.delete.mockResolvedValue({
+				where: {
+					id: mockUser.id,
+				},
+			});
+
+			if (!user || dto.token === 'invalidToken') {
+				throw new BadRequestException('There was an error!');
+			}
+
+			return { message: 'Account deleted' };
 		}),
 	};
 
@@ -58,21 +136,21 @@ describe('AuthController', () => {
 
 	it('should signup', async () => {
 		const dto: SignUpDto = {
-			email: 'newUser@gmail.com',
-			username: 'newUser',
-			password: 'newUser123',
+			email: 'IoannisNew@gmail.com',
+			username: 'IoannisNew',
+			password: 'Ioannis7',
 		};
 
 		const response = await controller.signup(dto);
-		expect(response).toEqual({ accessToken: '' });
+		expect(response).toEqual({ accessToken: 'mockedToken' });
 		expect(mockAuthService.signup).toHaveBeenCalledWith(dto);
 	});
 
 	it('should throw PrismaClientKnownRequestError if email is already registered', async () => {
 		const dto: SignUpDto = {
-			email: 'registeredUser@gmail.com',
-			username: 'registeredUser',
-			password: 'registeredUser123',
+			email: 'Ioannis@gmail.com',
+			username: 'Ioannis',
+			password: 'Ioannis7',
 		};
 
 		await expect(controller.signup(dto)).rejects.toThrow(BadRequestException);
@@ -80,11 +158,75 @@ describe('AuthController', () => {
 
 	it('should throw BadRequestException for other errors', async () => {
 		const dto: SignUpDto = {
-			email: 'invalid-email',
+			email: 'invalidEmail',
 			username: 'InvalidUser',
 			password: 'InvalidUser123',
 		};
 
 		await expect(controller.signup(dto)).rejects.toThrow(BadRequestException);
+	});
+
+	it('should signin', async () => {
+		const dto: SignInDto = {
+			email: 'Ioannis@gmail.com',
+			password: 'Ioannis7',
+		};
+
+		const response = await controller.signin(dto);
+		expect(response).toEqual({ accessToken: 'mockedToken' });
+		expect(mockAuthService.signin).toHaveBeenCalledWith(dto);
+	});
+
+	it('should throw BadRequestException for incorrect credentials', async () => {
+		const dto: SignInDto = {
+			email: 'invalidEmail',
+			password: 'InvalidUser123',
+		};
+
+		await expect(controller.signin(dto)).rejects.toThrow(BadRequestException);
+	});
+
+	it('should update', async () => {
+		const dto: UpdateDto = {
+			newEmail: 'IoannisUpdate@gmail.com',
+			newUsername: 'IoannisUpdate',
+			newPassword: 'Ioannis7',
+			token:
+				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQwMjA1Yzc5LTM5MDAtNDI3OC1hMzk2LWMzOWVlNjJiZmU1NyIsImVtYWlsIjoiSW9hbm5pc0BnbWFpbC5jb20iLCJ1c2VybmFtZSI6IklvYW5uaXMiLCJoYXNoIjoiJGFyZ29uMmlkJHY9MTkkbT02NTUzNix0PTMscD00JENPYVQwdmJoakNFVHdxOC9iNTAzS2ckT1NJeExpeTRQSERtSzJESXdCUy80bForcU4xU3RRSEFxN3Y5SzdqNGlqRSIsImNyZWF0ZWRBdCI6IjIwMjQtMDMtMTZUMTg6MTA6MjEuODI4WiIsInJvbGUiOiJQUkVNSVVNIiwiaWF0IjoxNzEwNjc3ODA3LCJleHAiOjE3MTA2ODUwMDd9.D0NtBxxdtY1aYZducH6nedcTAMGRstnClwCilepAXdM',
+		};
+
+		const response = await controller.update(dto);
+		expect(response).toEqual({ accessToken: 'mockedToken' });
+		expect(mockAuthService.update).toHaveBeenCalledWith(dto);
+	});
+
+	it('should throw BadRequestException for other errors', async () => {
+		const dto: UpdateDto = {
+			newEmail: 'invalidEmail',
+			newUsername: 'invalidUser',
+			newPassword: 'InvalidUser123',
+			token: 'invalidToken',
+		};
+
+		await expect(controller.update(dto)).rejects.toThrow(BadRequestException);
+	});
+
+	it('should delete', async () => {
+		const dto: TokenDto = {
+			token:
+				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQwMjA1Yzc5LTM5MDAtNDI3OC1hMzk2LWMzOWVlNjJiZmU1NyIsImVtYWlsIjoiSW9hbm5pc0BnbWFpbC5jb20iLCJ1c2VybmFtZSI6IklvYW5uaXMiLCJoYXNoIjoiJGFyZ29uMmlkJHY9MTkkbT02NTUzNix0PTMscD00JENPYVQwdmJoakNFVHdxOC9iNTAzS2ckT1NJeExpeTRQSERtSzJESXdCUy80bForcU4xU3RRSEFxN3Y5SzdqNGlqRSIsImNyZWF0ZWRBdCI6IjIwMjQtMDMtMTZUMTg6MTA6MjEuODI4WiIsInJvbGUiOiJQUkVNSVVNIiwiaWF0IjoxNzEwNjc3ODA3LCJleHAiOjE3MTA2ODUwMDd9.D0NtBxxdtY1aYZducH6nedcTAMGRstnClwCilepAXdM',
+		};
+
+		const response = await controller.delete(dto);
+		expect(response).toEqual({ message: 'Account deleted' });
+		expect(mockAuthService.delete).toHaveBeenCalledWith(dto);
+	});
+
+	it('should throw BadRequestException for other errors', async () => {
+		const dto: TokenDto = {
+			token: 'invalidToken',
+		};
+
+		await expect(controller.delete(dto)).rejects.toThrow(BadRequestException);
 	});
 });
--- nest-api/test/tests/auth.service.int.spec.ts
+++ nest-api/test/tests/auth.service.int.spec.ts
@@ -1,9 +1,8 @@
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
-import { access } from 'fs';
 import { AuthService } from 'src/auth/auth.service';
-import { SignUpDto } from 'src/auth/dto';
+import { SignInDto, SignUpDto } from 'src/auth/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('AuthService', () => {
@@ -12,10 +11,12 @@ describe('AuthService', () => {
 	const mockPrismaService = {
 		user: {
 			create: jest.fn(),
+			findUnique: jest.fn(),
 		},
 	};
 
 	const mockJwtService = {
+		jwt: jest.fn(),
 		signAsync: jest.fn(),
 	};
 
@@ -42,16 +43,16 @@ describe('AuthService', () => {
 
 	it('should create user and return access token', async () => {
 		const dto: SignUpDto = {
-			email: 'newUser@gmail.com',
-			username: 'newUser',
-			password: 'newUser123',
+			email: 'IoannisNew@gmail.com',
+			username: 'IoannisNew',
+			password: 'Ioannis7',
 		};
 
 		const mockUser = {
 			id: 'someUserId',
 			email: dto.email,
 			username: dto.username,
-			hash: 'hashedPassword',
+			hash: '$argon2id$v=19$m=65536,t=3,p=4$COaT0vbhjCETwq8/b503Kg$OSIxLiy4PHDmK2DIwBS/4lZ+qN1StQHAq7v9K7j4ijE',
 			createdAt: undefined,
 			role: 'FREE',
 		};
@@ -70,6 +71,52 @@ describe('AuthService', () => {
 				email: dto.email,
 				username: dto.username,
 				hash: expect.any(String),
+			},
+		});
+		expect(mockJwtService.signAsync).toHaveBeenCalledWith(
+			{
+				id: mockUser.id,
+				email: mockUser.email,
+				username: mockUser.username,
+				hash: mockUser.hash,
+				createdAt: mockUser.createdAt,
+				role: mockUser.role,
+			},
+			{
+				expiresIn: '120m',
+				secret: 'mockJwtSecret',
+			},
+		);
+		expect(mockConfigService.get).toHaveBeenCalledWith('JWT_SECRET');
+	});
+
+	it('should find user and return access token', async () => {
+		const dto: SignInDto = {
+			email: 'Ioannis@gmail.com',
+			password: 'Ioannis7',
+		};
+
+		const mockUser = {
+			id: 'someUserId',
+			email: dto.email,
+			username: 'Ioannis',
+			hash: '$argon2id$v=19$m=65536,t=3,p=4$COaT0vbhjCETwq8/b503Kg$OSIxLiy4PHDmK2DIwBS/4lZ+qN1StQHAq7v9K7j4ijE',
+			createdAt: undefined,
+			role: 'FREE',
+		};
+
+		const mockToken = 'mockAccessToken';
+
+		mockPrismaService.user.findUnique.mockResolvedValue(mockUser);
+		mockJwtService.signAsync.mockResolvedValue({ accessToken: mockToken });
+		mockConfigService.get.mockReturnValue('mockJwtSecret');
+
+		const result = await service.signin(dto);
+
+		expect(result.accessToken).toEqual({ accessToken: mockToken });
+		expect(mockPrismaService.user.findUnique).toHaveBeenCalledWith({
+			where: {
+				email: dto.email,
 			},
 		});
 		expect(mockJwtService.signAsync).toHaveBeenCalledWith(
